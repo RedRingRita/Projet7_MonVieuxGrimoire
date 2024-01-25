b243a7ab8f8bd20a453ed776e855d2c28149fa82
Prunelle <zhavina@protonmail.com>
2024-01-25 17:26:43 +0100

    Singup et Login fonctionnels.

--- controllers/user.js
+++ controllers/user.js
@@ -3,31 +3,45 @@ const bcrypt = require('bcrypt');
 const jwt = require('jsonwebtoken');
 require('dotenv').config();
 
+//Code qui ne fonctionnait pas :
+// exports.signup = (req, res, next) => {
+//     bcrypt.hash(req.body.password, 10)
+//     .then(hash => {
+//         const user = new User({
+//             email : req.body.email,
+//             password : hash,
+//         });        
+//         user.save()
+//             .then(() => res.status(201).json({message : 'Utilisateur créé'}))
+//             .catch(res.status(400).json({ message : "Erreur lors de l'inscription" }));
+//     })
+//     .catch(res.status(500).json({ message : "Erreur lors de l'inscription" }));
+// };
+
 exports.signup = (req, res, next) => {
     bcrypt.hash(req.body.password, 10)
-    .then(hash => {
+      .then(hash => {
         const user = new User({
-            email : req.body.email,
-            password : hash,
+          email: req.body.email,
+          password: hash
         });
-        console.log("Le code arrive bien ici");
         user.save()
-            .then(() => res.status(201).json({message : 'Utilisateur créé'}))
-            .catch(res.status(400).json({ message : "Erreur lors de l'inscription" }));
-    })
-    .catch(res.status(500).json({ message : "Erreur lors de l'inscription" }));
-};
+          .then(() => res.status(201).json({ message: 'Utilisateur créé !' }))
+          .catch(error => res.status(400).json({ message : "Erreur lors de l'inscription", error: error.message }));
+      })
+      .catch(error => res.status(500).json({ message : "Erreur lors de l'inscription", error: error.message }));
+  };
 
 exports.login = (req, res, next) => {
     User.findOne({email : req.body.email})
         .then((user) => { /*user est la valeur qui est retournée par la method findOne*/
             if (!user) {
-                return res.status(401).json({ message : 'Login ou mot de passe incorrect'})
+                return error => res.status(401).json({ message : 'Login ou mot de passe incorrect', error: error.message})
             }
             bcrypt.compare(req.body.password, user.password)
                 .then(valid => {
                     if (!valid) {
-                        return res.status(401).json({ message : 'Login ou mot de passe incorect'})
+                        return error => res.status(401).json({ message : 'Login ou mot de passe incorect', error: error.message })
                     }
                     res.status(200).json({
                         userId : user._id, /*_id est créé par la BDD lors du POST au user*/
@@ -39,5 +53,5 @@ exports.login = (req, res, next) => {
                     })
                 })
         })
-        .catch(res.status(500).json({ message : "Erreur lors du login" }));
+        .catch(error => res.status(500).json({ message : "Erreur lors du login", error: error.message }));
 };
